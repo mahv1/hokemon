0f1fbdc85b2cb741d6d1fa572062650a9a9a4ed3
Mahlulwa Vuyelele <VUY202001069@longroad.ac.uk>
2021-01-20 11:49:32 +0000

    Add project files.

---
+++ hokemon/hokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hokemon
+{
+    class hokemon  
+    {
+        private string name;
+        private int health;
+        private int max_health;
+        private int attack;
+        private int speed;
+        private int defence;
+
+    }
+}
