ce538c9853f1d68d5c4d8bdb17ee71bff9e4ae3c
Gareth Adams <gadams@longroad.ac.uk>
2021-01-20 12:01:54 +0000

    Cathcup!!

--- hokemon/Program.cs
+++ hokemon/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace hokemon
+namespace Hokemon
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello welcome to Hokeworld home of the hokemon!");
+
+            // Instantiation of new hokemon
+
+            hokemon hoke01 = new hokemon();  // INSTANTIATE new hokemon object referred to as Hoke01
+
+            hoke01.get_details();
+
+            hokemon hoke02 = new hokemon();  // INSTANTIATE Hoke02
+
+            hoke02.get_details();
+
         }
     }
 }
--- hokemon/hokemon.cs
+++ hokemon/hokemon.cs
@@ -6,12 +6,80 @@ namespace Hokemon
 {
     class hokemon  
     {
+        // MEMBERS - All Attributes, Methods and Properties are MEMBERS of this CLASS
+
+        // VARIABLES a.k.a ATTRIBUTES inside a class can also be refered to as FIELDS
         private string name;
         private int health;
         private int max_health;
         private int attack;
         private int speed;
-        private int defence;
+        private int defense;
 
+        // PROPERTIES
+        // PROPERTIES are a combination of a field and method - this keeps variables / attributes PRIVATE
+        // and allows us to perform checks on what is input
+        public string Name         // Notice PROPERTY has no parenthesis its identifier unlike the METHODS below
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Health         // Health PROPERTY this is public keeping the 'health' 
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        // METHODS
+        public hokemon()  // CONSTRUCTOR method 
+        {
+            provide_name();
+            max_health = 100;
+            health = random_int_generator(10, max_health);
+            attack = random_int_generator(10, 100);
+            speed = random_int_generator(10, 100);
+            defense = random_int_generator(10, 100);
+
+        }
+
+        public void get_details()
+        {
+            Console.WriteLine("\n\n************\nName: {0}\nHealth: {1}/{2}\nAttack: {3}\nSpeed: {4}\nDefense: {5}\n**********\n", name, health, max_health, attack, speed, defense);
+        }         // get_details METHOD 
+
+        public void provide_name()
+        {
+            Console.WriteLine("Enter a name for your Hokemon: ");
+            name = Console.ReadLine();
+        }
+
+        public int random_int_generator(int min_value, int max_value) // PARAMETERS int min_value, int max_value
+        {
+            Random rnd = new Random(); // INSTANTIATING the rnd object from the RANDOM class 
+
+            int randomValue;
+
+            randomValue = rnd.Next(min_value, max_value);
+
+            return randomValue;
+        }
+
+        public int attackCalculator()
+        {
+            int attackValue;
+            Random rnd = new Random();
+            attackValue = ((attack * speed) / 2) * rnd.Next(0, 2); //
+
+            return attackValue;
+        }
+        public int defenceCalculator()
+        {
+            int defenceValue;
+            Random rnd = new Random();
+
+            defenceValue = (defense * speed) / 10 * rnd.Next(0, 2);
+
+            return defenceValue;
+        }
     }
 }
